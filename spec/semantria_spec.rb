f053d35b298e5a842184f12c49635f5db8346112
Mikhail Dubov <mdubov@gmail.com>
2014-08-05 21:33:27 +0100

    Additional specs for client

--- spec/semantria_spec.rb
+++ spec/semantria_spec.rb
@@ -5,15 +5,24 @@ describe Semantria::Client do
     it 'include httparty methods' do
       expect(described_class).to include HTTParty
     end
+
     it "must have the base url set to the Semantria API endpoint" do
       expect(described_class.base_uri).to eq 'https://api35.semantria.com'
     end
+
     it 'initialized with key and secret' do
       expect(described_class.new('bla', 'bla')).to respond_to :consumer_key
       expect(described_class.new('bla', 'abla').consumer_key).to eq 'bla'
       expect(described_class.new('bla', 'bla')).to respond_to :consumer_secret
       expect(described_class.new('bla', 'abla').consumer_secret).to eq 'abla'
     end
+
+    it 'has an auth object on initialization' do
+      expect(described_class.new('bla', 'bla')).to respond_to :auth
+      expect(described_class.new('bla', 'bla').auth).to be_a Semantria::Authenticator
+    end
+
+
     it 'has default headers' do
       expect(described_class.new('bla', 'bla').headers).to be_a Hash
       expect(described_class.new('bla', 'bla').headers).not_to be_empty
